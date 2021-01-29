49725bc147e7974914d9fd113b0beb65278f5c79
Senne Van Steelandt <sennevs@gmail.com>
2021-01-28 23:55:15 -0500

    fix gitignore

---
+++ games/twoseventy/components/__init__.py
@@ -0,0 +1,3 @@
+from .board import Board
+from .candidate import Candidate
+from .player import Player
---
+++ games/twoseventy/data/__init__.py
@@ -0,0 +1,4 @@
+from .candidates import candidate_data
+from .state_groups import state_group_data
+from .national_groups import national_group_data
+from .states import state_data
